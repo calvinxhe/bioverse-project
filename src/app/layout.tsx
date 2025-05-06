1541973feb23031f78b926bed6d2f3d54a2570c1
calvinxhe <130882198+calvinxhe@users.noreply.github.com>
2025-05-05 21:16:58 -0400

    home page changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,24 +2,33 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { AuthProvider } from '@/components/context/auth-context';
+import { Box } from '@mui/material';
+import ClientLayout from '@/components/layout/client-layout';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bioverse Questionnaire System",
-  description: "A questionnaire system for Bioverse",
+	title: "Bioverse Questionnaire System",
+	description: "A questionnaire system for Bioverse",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<Providers>
+					<AuthProvider>
+						<ClientLayout>
+							{children}
+						</ClientLayout>
+					</AuthProvider>
+				</Providers>
+			</body>
+		</html>
+	);
 }
