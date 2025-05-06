175e56b2ddc060f55304bdeddf676e94b29afb40
calvinxhe <130882198+calvinxhe@users.noreply.github.com>
2025-05-05 21:23:53 -0400

    fix ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,12 +6,7 @@ import LoginForm from '@/components/pages/login-page';
 export default function Login() {
 	return (
 		<Box sx={{ p: 3, display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-			<Paper sx={{ p: 4, maxWidth: 400, width: '100%' }}>
-				<Typography variant="h4" component="h1" gutterBottom align="center">
-					Login
-				</Typography>
 				<LoginForm />
-			</Paper>
 		</Box>
 	);
 }
